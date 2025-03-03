cc45ebcefeaed18d76cdf73d565cf8ea62cd5688
Johanna Nasr <if23b284@technikum-wien.at>
2025-03-03 15:30:35 +0100

    Main Outputs added

--- src/main/java/mtcg/server/ServerMain.java
+++ src/main/java/mtcg/server/ServerMain.java
@@ -16,17 +16,27 @@ import controller.TradingController;
 public class ServerMain {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(10001), 0);
-
+        System.out.println("Server instanziert");
         new UserController(server);
+        System.out.println("UserController neue Instanz");
         new SessionController(server);
+        System.out.println("SessionController neue Instanz");
         new PackageController(server);
+        System.out.println("PackageController neue Instanz");
         new TransactionController(server);
+        System.out.println("TransactionController neue Instanz");
         new CardController(server);
+        System.out.println("CardController neue Instanz");
         new DeckController(server);
+        System.out.println("DeckController neue Instanz");
         new StatsController(server);
+        System.out.println("StatsController neue Instanz");
         new ScoreboardController(server);
+        System.out.println("ScoreboardController instanziert");
         new BattleController(server);
+        System.out.println("BattleController instanziert");
         new TradingController(server);
+        System.out.println("TradingController instanziert");
 
         server.setExecutor(null);
         server.start();
